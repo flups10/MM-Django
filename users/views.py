3da7f722a0f9440db1a9d297e78647b00e3a69f4
flups10 <je.tieleman@gmail.com>
2023-11-16 12:17:42 +0100

    finished all requirements

--- users/views.py
+++ users/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from .models import User
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
+import uuid
+
+import datetime
 
 # Create your views here.
 def index(request):
@@ -9,29 +12,42 @@ def index(request):
 
 def register(request):
 
-    User.objects.get_or_create({
-            'name': request.POST['name'],
-            'email': request.POST['email'],
-            'password': request.POST['password'],
-            'job_title': request.POST['job_title']
-            })
+    try:
+        exists = User.objects.get(email = request.POST['email'])
+    except User.DoesNotExist:
+        leng = User.objects.all().count()
 
-    response = HttpResponse('test')
-    response.set_cookie('user_cookie', request.POST['email'])
-    return redirect('members')
+        user = User(
+                leng+1,
+                request.POST['name'],
+                request.POST['email'],
+                request.POST['job_title'],
+                request.POST['password'],
+                )
 
+        user.save()
+        response = HttpResponseRedirect('/members')
+        response.set_cookie('user_cookie', request.POST['email'])
+        return response
+    
+    
+    context = {
+            'register_message': 'user already exists'
+        }
+    return render(request, 'users/index.html', context)
 
 def login(request):
     try:
         user = User.objects.get(email = request.POST['email'])
+        if user.password == request.POST['password']:
+            pass
+        else:
+            return render(request, 'users/index.html', {'login_message': 'incorrect password'})
     except (User.DoesNotExist):
-        raise Http404("User does not exist")
-
-    print(request.POST['password'])
-
-    response = HttpResponse('test')
+        return render(request, 'users/index.html', {'login_message': 'user does not exist'})
+    response = redirect('members')
     response.set_cookie('user_cookie', request.POST['email'])
-    return redirect('members')
+    return response
 
 
 def logout(request):
@@ -39,14 +55,17 @@ def logout(request):
     response.delete_cookie('user_cookie')
     return response
 
+
 def members(request):
-    print(request.user)
-    if request.user == None:
-        redirect('/')
+    if request.COOKIES.get('user_cookie'):
+        pass
+    else:
+        return redirect('/')
     members = User.objects.all()
-    context = {'members': members}
+    context = {'members': members,
+               'user': request.COOKIES['user_cookie']}
     return render(request, 'users/members.html', context)
-    
+
 
 def profile(request, member_id):
     user = User.objects.get(pk=member_id)
@@ -56,3 +75,4 @@ def profile(request, member_id):
         'job_title': user.job_title
     }
     return render(request, 'users/profile.html', context)
+
